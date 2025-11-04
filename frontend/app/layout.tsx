ceb8dc5913a537432cb0a56979a6600a6bdc8eba
Otso Saarinen <saarinenotso@gmail.com>
2025-11-04 18:05:01 +0200

    add ModeToggle to layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -23,11 +26,21 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+					<div className="absolute top-3 right-3">
+						<ModeToggle />
+					</div>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
