1a094846c6b45f9648bbf5c7ee560b82a08621ce
Otso Saarinen <saarinenotso@gmail.com>
2025-11-04 18:33:10 +0200

    add basic text to homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,8 +16,34 @@ export default function Home() {
 	};
 
 	return (
-		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<Button onClick={ApiTest}>API</Button>
+		<div className="flex min-h-screen flex-col items-center justify-center gap-1 bg-zinc-50 p-3 text-center font-sans dark:bg-black">
+			<h1 className="text-7xl font-bold">
+				<span className="text-orange-500">Job</span> Tracker
+			</h1>
+			<h2 className="text-lg font-medium">
+				<span className="underline decoration-orange-500">Manage</span>
+				{", "}
+				<span className="underline decoration-orange-500">
+					organize
+				</span>{" "}
+				and{" "}
+				<span className="underline decoration-orange-500">track</span>{" "}
+				your job applications with{" "}
+				<span className="underline decoration-orange-500">
+					job tracker
+				</span>
+			</h2>
+			<h3 className="mt-10 text-lg font-medium">
+				Click{" "}
+				<span className="text-orange-500 underline">
+					<a href="/signup">here</a>
+				</span>{" "}
+				to sign up
+			</h3>
+			<div className="absolute top-3 left-3 flex flex-row gap-3 font-medium">
+				<a href="/login">Login</a>
+				<a href="/signup">Signup</a>
+			</div>
 		</div>
 	);
 }
